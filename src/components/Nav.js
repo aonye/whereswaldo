f7ba9b05c7af243ce11696711b7da80517adb6c8
Andy Ye <ye.andyy@gmail.com>
2021-09-14 22:04:37 -0400

    images added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,32 @@
 import PokeLogo from '../images/poke_crop.png';
+import { images } from './Ww1_images/index';
 import '../styles/Nav.css';
 
 const Nav = () => {
+    console.log(images);
     return (
         <nav>
             <div className='logoContainer'>
                 <img className='logoImg' src={PokeLogo} alt='Logo' />
                 <div>Search</div>
             </div>
-            <ul className='navlinks'>
-                <li>Home</li>
-                <li>Shop</li>
-                <li>About</li>
-                <li>XQC</li>
-            </ul>
+            <div>
+                <img src={images[0]} alt='' height='40' width='40' />
+                <img src={images[1]} alt='' height='40' width='40' />
+                <img src={images[2]} alt='' height='40' width='40' />
+            </div>
+            <div className='navLinks'>
+                <div>Counter</div>
+                <div>XQC</div>
+            </div>
         </nav >
     );
 };
+
+{/* <ul className='navlinks'>
+    <li>Home</li>
+    <li>Counter</li>
+    <li>XQC Trophy</li>
+</ul> */}
 
 export default Nav;
---
+++ src/components/Ww1_images/index.js
@@ -0,0 +1,13 @@
+import Blissey from './blissey.png';
+import Eevee from './eevee.png';
+import Chatot from './chatot.png';
+
+const images = {
+    0: Blissey,
+    1: Eevee,
+    2: Chatot,
+}
+
+export { images };
+
+
